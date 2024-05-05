f13d3fbc2de90768ce0d3fe12e01483790c6bbec
Mesrop Tarkhanyan <mesroptnamak@gmail.com>
2024-05-05 22:56:31 +0400

    Update

--- config.tf
+++ config.tf
@@ -6,16 +6,18 @@ terraform {
     }
   }
 
+# Backend (state файла хранится удалённо, например в s3)
   backend "s3" {
     bucket = "qnt-clouds-for-pe-tfstate"
-    key    = "mesrop_tarkhanyan/terraform.tfstate"
+    key    = "user_homework_1/terraform.tfstate"
     region = "us-east-2"
+    encrypt = true  # рекомендуется, так как state файл может хранить чувствительную информацию
+#     dynamodb_table = "terraform-state-lock-app" # чтобы state файл одновременно редактировал только один пользователь, но dynamodb_table должна быть создана заранее, terraform сам не создаст
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  alias      = "aws_us_east_2"
   region     = "us-east-2"
   access_key = var.access_key
   secret_key = var.secret_key
