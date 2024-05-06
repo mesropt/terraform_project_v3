99c9252ec3f337b03586ab5fc71276cce92ba3c9
Mesrop Tarkhanyan <mesroptnamak@gmail.com>
2024-05-06 19:30:40 +0400

    Update

--- config.tf
+++ config.tf
@@ -6,13 +6,14 @@ terraform {
     }
   }
 
-# Backend (state файла хранится удалённо, например в s3)
+  # 2.1. Declare remote state backend in s3
+  # Backend (state файла лучше хранить удалённо, например в s3)
   backend "s3" {
-    bucket = "qnt-clouds-for-pe-tfstate"
-    key    = "user_homework_1/terraform.tfstate"
-    region = "us-east-2"
-    encrypt = true  # рекомендуется, так как state файл может хранить чувствительную информацию
-#     dynamodb_table = "terraform-state-lock-app" # чтобы state файл одновременно редактировал только один пользователь, но dynamodb_table должна быть создана заранее, terraform сам не создаст
+    bucket  = "qnt-clouds-for-pe-tfstate"
+    key     = "user_homework_1/terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true # рекомендуется, так как state файл может хранить чувствительную информацию
+    # dynamodb_table = "terraform-state-lock-app" # чтобы state файл одновременно редактировал только один пользователь, но dynamodb_table должна быть создана заранее, так как terraform сам его не создаст
   }
 }
 
