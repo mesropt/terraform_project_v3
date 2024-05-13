b4d566236de9d2c3bbd01d0ee269088ace887748
Mesrop Tarkhanyan <mesroptnamak@gmail.com>
2024-05-13 14:40:39 +0400

    Update project

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
-# variable "access_key" {
-#   description = "AWS Access Key"
-#   type        = string
-#   sensitive   = true
-# }
-#
-# variable "secret_key" {
-#   description = "AWS Secret Key"
-#   type        = string
-#   sensitive   = true
-# }
+variable "access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
 
 variable "common_tags" {
   default = {
